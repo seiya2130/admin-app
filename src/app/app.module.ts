67df8ef98806b5f9982bc85b8e1f78184520e460
Seiya_Satoh <satohseiya0312@gmail.com>
2022-03-25 17:12:06 +0900

    [追加]メンバコンポーネント追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { MembersComponent } from './members/members.component';
+import { MemberDetailComponent } from './member-detail/member-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MembersComponent,
+    MemberDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/members/members.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Member } from '../member';
+import { MEMBERS } from '../mock-members';
+
+@Component({
+  selector: 'app-members',
+  templateUrl: './members.component.html',
+  styleUrls: ['./members.component.css']
+})
+export class MembersComponent implements OnInit {
+
+  members = MEMBERS;
+  member: Member = {
+    id: 1,
+    name: '田中太郎'
+  };
+  selectedMember: Member;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(member: Member): void {
+    this.selectedMember = member;
+  }
+
+
+
+}
